a6cba3664c8a30c9bc75a9f4b6c8c1e1f51ea2d0
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2017-10-30 20:46:32 -0200

    numeros parte 2

--- unidade_05/numeros2.php
+++ unidade_05/numeros2.php
@@ -1,6 +1,7 @@
 <?php
     $salario = 800;
-    $gasolina = 2.79;
+    $gasolina = 2.7954;
+    $nome = "Luís"
 ?>
 
 <!doctype html>
@@ -11,18 +12,29 @@
     </head>
 
     <body>
-        
-        
+
+
         <?php
             // testar se é numérica
+            echo "O $salario é um número?" . is_numeric($salario) . "<br>";
+            echo "O $gasolina é um número?" . is_numeric($gasolina) . "<br>";
+            echo "O $nome é um número?" . is_numeric($nome) . "<br>";
 
 
             // testar se é inteiro
-
+            echo "O $salario é um inteiro?" . is_int($salario) . "<br>";
+            echo "O $gasolina é um inteiro?" . is_int($gasolina) . "<br>";
 
             // testar se é float
+            echo "O $salario é um float?" . is_float($salario) . "<br>";
+            echo "O $gasolina é um float?" . is_float($gasolina) . "<br>";
+
+            //arredondar
+            echo round($gasolina) . "<br>";
+            echo floor($gasolina) . "<br>";
+            echo ceil($gasolina) . "<br>";
         ?>
-        
-        
+
+
     </body>
 </html>
