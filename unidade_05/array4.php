dbd8a736eb03c94fb26cb38a830ef9ccc07aa16d
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2017-10-30 21:04:21 -0200

    Array 4

---
+++ unidade_05/array4.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Curso PHP FUNDAMENTAL</title>
+    </head>
+
+    <body>
+      <?php
+        $salada = array("Maçã", "Abacate", "Laranja");
+
+        echo "Existe o Uva nesse array?? " . array_search("Uva", $salada) . "<br>";
+      ?>
+    </body>
+</html>
