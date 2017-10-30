50f0a2e440f6edd2bea8da4f91a39dcf448e59a2
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2017-10-30 20:18:02 -0200

    variaveis php

--- unidade_04/inicial.php
+++ unidade_04/inicial.php
@@ -5,6 +5,11 @@
         <title>Curso PHP FUNDAMENTAL</title>
     </head>
     <?php
+        //comentario1
+        #comentario2
+        /*
+        comentario 3
+         */
         echo "Luís Felipe Bilecki";
      ?>
     <body>
---
+++ unidade_05/variaveis.php
@@ -0,0 +1,17 @@
+<?php
+  $_nome = "Luís";
+  $_salario = 3000;
+  ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title></title>
+  <link rel="stylesheet" href="">
+</head>
+<body>
+      <p><?php echo "Nome: $_nome" ?></p>
+      <p><?php echo "Salario: $_salario" ?></p>
+</body>
+</html>
