1c9077752e03dfdb23b5c2bed37ecdda5c52e408
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2017-10-30 20:20:47 -0200

    concatenacao de strings

---
+++ unidade_05/string1.php
@@ -0,0 +1,22 @@
+<?php
+    $_nome = "Maria";
+    $_sobrenome = "Silva";
+    $_nomecompleto = $_nome." ".$_sobrenome;
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Curso PHP FUNDAMENTAL</title>
+    </head>
+
+    <body>
+        <?php
+            echo $_nome . " " . $_sobrenome . "<br>";
+            echo $_nomecompleto;
+
+
+         ?>
+    </body>
+</html>
