467517dc9e511c6a023bb6be43119ddaa3a65ea6
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2017-10-30 20:34:13 -0200

    algumas funcoes de string

--- unidade_05/string2.php
+++ unidade_05/string2.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
     $_nome = "Curso PHP Fundamental";
 ?>
 
@@ -10,29 +10,34 @@
     </head>
 
     <body>
-        
-        <?php 
+
+        <?php
             // strlen - Retorna primeira ocorrencia
-            
-            
-            // stripos  - Retorna primeira ocorrência 
-            
+            echo strlen("Brasil")."</br>";
+
+            // stripos  - Retorna primeira ocorrência
+            echo stripos($_nome, "u")."</br>";
 
             // strripos - Retorna última ocorrência
-            
-            
+            echo strripos($_nome, "u")."</br>";
+
             // strtolower - converte para letras min.
-            
+            echo strtolower($_nome)."</br>";
+
 
             // strtoupper - converte para letras min.
-            
+            echo strtoupper($_nome)."</br>";
+
 
             // SUBSTR_COUNT - Conta quantas ocorreram
             // de um texto ou string
             // Faz diferença Maiusculas e minusculas
-            
+            echo SUBSTR_COUNT($_nome,"P")."</br>";
+
+            //outras funções de string
+            //http://php.net/manual/pt_BR/ref.strings.php
+
         ?>
-        
-        
+
     </body>
 </html>
