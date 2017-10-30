f301d436b731ee7fc0e22815a2ebe485de4399e1
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2017-10-30 20:40:50 -0200

    funcoes de numericas

--- unidade_05/numeros1.php
+++ unidade_05/numeros1.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
     $salario = 800;
     $meses   = 3;
 ?>
@@ -11,19 +11,28 @@
     </head>
 
     <body>
-        <?php 
+        <?php
             // Multiplicacao e Divisao
+            echo "Trimestre: " . ($salario * $meses) . "</br>";
+            echo "Quinzena: " . ($salario/$meses) . "</br>";
 
             // Exponencial
+            echo "8^2 = " . pow(8,2) . "</br>";
 
             // Raiz Quadrada
+            echo "Raiz quadrada de 36 = " . sqrt(36) . "</br>";
 
             // Randômico Generica
+            echo "Um número aleatório qualquer: " . rand() . "</br>";
 
             // Randômico entre um intervalo
+            echo "Um número aleatório entre 1 e 5: " . rand(1,5) . "</br>";
 
             // Valor absoluto
-            
+            echo "Valor absoluto de -10 é ". abs(-10);
+
+            // Outras funções numéricas
+            //https://secure.php.net/manual/pt_BR/book.math.php
         ?>
     </body>
 </html>
