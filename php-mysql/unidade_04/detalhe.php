5e1229c0d6609ecc458eccfe85f947b5261db102
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2017-10-31 21:22:28 -0200

    detalhe do produto - parte 2

--- php-mysql/unidade_04/detalhe.php
+++ php-mysql/unidade_04/detalhe.php
@@ -17,6 +17,15 @@
     }
     else{
         $dados_detalhe = mysqli_fetch_assoc($detalhe);
+        $produtoID = $dados_detalhe["produtoID"];
+        $nomeproduto = $dados_detalhe["nomeproduto"];
+        $descricao = $dados_detalhe["descricao"];
+        $codigobarra = $dados_detalhe["codigobarra"];
+        $tempoentrega = $dados_detalhe["tempoentrega"];
+        $precorevenda = $dados_detalhe["precorevenda"];
+        $precounitario = $dados_detalhe["precounitario"];
+        $estoque = $dados_detalhe["estoque"];
+        $imagemgrande = $dados_detalhe["imagemgrande"];
     }
 ?>
 <!doctype html>
@@ -28,13 +37,25 @@
         <!-- estilo -->
         <link href="_css/estilo.css" rel="stylesheet">
         <link href="_css/produtos.css" rel="stylesheet">
+        <link href="_css/produto_detalhe.css" rel="stylesheet">
     </head>
 
     <body>
         <?php include_once("_incluir/topo.php") ?>
 
         <main>
-            <?php print_r($dados_detalhe); ?>
+            <div id="detalhe_produto">
+                <ul>
+                    <img src="<?php echo $imagemgrande ?>">
+                    <li> <h2><?php echo $nomeproduto ?></h2></li>
+                    <li>  <b>Descrição: </b> <?php echo $descricao ?></li>
+                    <li> <b>Código de Barra: </b> <?php echo $codigobarra ?></li>
+                    <li> <b>Tempo de Entrega: </b><?php echo $tempoentrega ?></li>
+                    <li> <b>Preço de Revenda: </b><?php echo "R$ " .  number_format($precorevenda,2,",",".") ?></li>
+                    <li> <b>Preço Unitário: </b><?php echo "R$ " .  number_format($precounitario,2,",",".") ?></li>
+                    <li> <b>Quantidade em Estoque: </b><?php echo $estoque ?></li>
+                </ul>
+            </div>
         </main>
 
         <?php include_once("_incluir/rodape.php") ?>
