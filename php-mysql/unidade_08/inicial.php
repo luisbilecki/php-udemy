5215ffc7c00785d0c62780b5a6c1eab28b7a0731
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2017-11-02 20:09:30 -0200

    Insercao no banco

--- php-mysql/unidade_08/inicial.php
+++ php-mysql/unidade_08/inicial.php
@@ -1,23 +1,23 @@
-<?php require_once("../../conexao/conexao.php"); ?>
+<?php require_once("../conexao/conexao.php"); ?>
 
 <!doctype html>
 <html>
     <head>
         <meta charset="UTF-8">
         <title>Curso PHP INTEGRACAO</title>
-        
+
         <!-- estilo -->
         <link href="_css/estilo.css" rel="stylesheet">
     </head>
 
     <body>
         <?php include_once("_incluir/topo.php"); ?>
-        
-        <main>  
-            
+
+        <main>
+
         </main>
 
-        <?php include_once("_incluir/rodape.php"); ?>  
+        <?php include_once("_incluir/rodape.php"); ?>
     </body>
 </html>
 
